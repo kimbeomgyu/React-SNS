5b3366b37341d1acf9e20e2ab33570b6e10a698e
kimbeomgyu <qjarb1994@gmail.com>
2019-05-28 20:12:08 +0900

    middleware 셋팅 redux devtools extension 적용

--- react-sns-front/pages/_app.js
+++ react-sns-front/pages/_app.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 import PropTypes from "prop-types";
 import AppLayout from "../components/AppLayout";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import withRedux from "next-redux-wrapper";
 import reducer from "../reducers";
 
@@ -32,7 +32,13 @@ App.propTypes = {
 };
 
 export default withRedux((initialState, options) => {
-  const store = createStore(reducer, initialState);
   const middlewares = [];
+  const enhancer = compose(
+    applyMiddleware(...middlewares),
+    !options.isServer && window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined"
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f
+  );
+  const store = createStore(reducer, initialState, enhancer);
   return store;
 })(App);
