4d9c5818bf16cd34e12512931222752d23bad2f6
kimbeomgyu <qjarb1994@gmail.com>
2019-05-28 11:15:24 +0900

    프로필 페이지 팔로워,팔로잉 목록 dummy 목업 구현

--- react-sns-front/pages/profile.js
+++ react-sns-front/pages/profile.js
@@ -1,7 +1,53 @@
 import React from "react";
+import { Form, Input, Button, List, Card, Icon } from "antd";
 
 const Profile = () => {
-  return <div>사용자</div>;
+  return (
+    <div>
+      <Form
+        style={{
+          marginBottom: "20px",
+          border: "1px solid #d9d9d9",
+          padding: "20px"
+        }}
+      >
+        <Input addonBefore="닉네임" />
+        <Button type="primary">수정</Button>
+      </Form>
+      <List
+        style={{ marginBottom: "20px" }}
+        grid={{ gutter: 4, xs: 2, md: 3 }}
+        size="small"
+        header={<div>팔로워 목록</div>}
+        loadMore={<Button style={{ width: "100%" }}>더 보기</Button>}
+        bordered
+        dataSource={["wp1rndur", "pjj0714", "doheekang"]}
+        renderItem={item => (
+          <List.Item style={{ marginTop: "20px" }}>
+            <Card actions={[<Icon type="stop" key="stop" />]}>
+              <Card.Meta description={item} />
+            </Card>
+          </List.Item>
+        )}
+      />
+      <List
+        style={{ marginBottom: "20px" }}
+        grid={{ gutter: 4, xs: 2, md: 3 }}
+        size="small"
+        header={<div>팔로잉 목록</div>}
+        loadMore={<Button style={{ width: "100%" }}>더 보기</Button>}
+        bordered
+        dataSource={["wp1rndur", "pjj0714", "doheekang"]}
+        renderItem={item => (
+          <List.Item style={{ marginTop: "20px" }}>
+            <Card actions={[<Icon type="stop" key="stop" />]}>
+              <Card.Meta description={item} />
+            </Card>
+          </List.Item>
+        )}
+      />
+    </div>
+  );
 };
 
 export default Profile;
