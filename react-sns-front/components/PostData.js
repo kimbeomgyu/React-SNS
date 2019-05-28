292284968de01d3607cd81165688e509394fa8ca
kimbeomgyu <qjarb1994@gmail.com>
2019-05-28 13:13:01 +0900

    PostData 수정 구조분해, 타입지정 상세화

--- react-sns-front/components/PostData.js
+++ react-sns-front/components/PostData.js
@@ -2,11 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Button, Icon, Card, Avatar } from "antd";
 
-const PostData = ({ data }) => {
+const PostData = ({ data: { createdAt, img, User, content } }) => {
   return (
     <Card
-      key={+data.createdAt}
-      cover={data.img && <img alt="example" src={data.img} />}
+      key={+createdAt}
+      cover={img && <img alt="example" src={img} />}
       actions={[
         <Icon type="retweet" key="retweet" />,
         <Icon type="heart" key="heart" />,
@@ -16,16 +16,21 @@ const PostData = ({ data }) => {
       extra={<Button>팔로우</Button>}
     >
       <Card.Meta
-        avatar={<Avatar>{data.User.nickname[0]}</Avatar>}
-        title={data.User.nickname}
-        description={data.content}
+        avatar={<Avatar>{User.nickname[0]}</Avatar>}
+        title={User.nickname}
+        description={content}
       />
     </Card>
   );
 };
 
 PostData.propTypes = {
-  data: PropTypes.object
+  data: PropTypes.shape({
+    User: PropTypes.object,
+    content: PropTypes.string,
+    img: PropTypes.string,
+    createdAt: PropTypes.object
+  })
 };
 
 export default PostData;
